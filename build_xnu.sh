6411a89529e80211c5300a113e231e560bc83adf
lyricalsoul <lyricalsoul@proton.me>
2023-07-02 15:11:54 -0400

    add ctf tools to build, upload built kernel, remove libplatform headers

--- build_xnu.sh
+++ build_xnu.sh
@@ -2,8 +2,6 @@
 # This script is based on zhouwei's build-xnu-6153.141.1.sh.
 # For further information, refer to https://gist.github.com/zhuowei/69c886423642cd77fd2c010f4d54b1c4
 
-export DEVELOPER_DIR="/Applications/Xcode_14.3.1.app/Contents/Developer"
-
 if ! [[ $OSTYPE == 'darwin'* ]]; then
   echo 'This command can only be ran on a macOS host. Sorry!'
   exit 1
@@ -15,52 +13,44 @@ umask 022
 # Print commands and exit on failure.
 set -ex
 
-# Get the SDK path.
-SDKPATH="$DEVELOPER_DIR/Platforms/MacOSX.platform/Developer/SDKs/MacOSX13.3.sdk"
-[ -d "${SDKPATH}" ]
+export DEVELOPER_DIR="/Applications/Xcode_15.0.app/Contents/Developer"
+export SDKPATH="$DEVELOPER_DIR/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.0.sdk"
+export TOOLCHAINPATH="$SDKPATH/../../Toolchains/XcodeDefault.xctoolchain"
+[[ -d "${SDKPATH}" ] && [ -d "${TOOLCHAINPATH}" ]
+
+# Install CTF tools from dtrace
+cd dtrace
+xcodebuild install -sdk macosx -target ctfconvert -target ctfdump -target ctfmerge ARCHS='x86_64' VALID_ARCHS='x86_64' SRCROOT="${PWD}" OBJROOT="${PWD}/obj" SYMROOT="${PWD}/sym" DSTROOT="${PWD}/dst"
+sudo ditto "${PWD}/dst/${TOOLCHAINPATH}" "${TOOLCHAINPATH}"
+cd ..
 
 # Install AvailabilityVersions. This writes to ${SDKPATH}/usr/local/libexec.
 cd AvailabilityVersions
 mkdir -p dst
-make install SRCROOT="${PWD}" DSTROOT="${PWD}/dst"
+make install
 sudo ditto "${PWD}/dst/usr/local" "${SDKPATH}/usr/local"
 cd ..
 
-# Install the XNU headers we'll need for libdispatch. This OVERWRITES files in the specified MacOSX SDK.
+# Install the XNU headers needed by libdispatch.
 cd xnu
-mkdir -p BUILD.hdrs/obj BUILD.hdrs/sym BUILD.hdrs/dst
-make installhdrs SDKROOT=macosx ARCH_CONFIGS=X86_64 SRCROOT="${PWD}" OBJROOT="${PWD}/BUILD.hdrs/obj" SYMROOT="${PWD}/BUILD.hdrs/sym" DSTROOT="${PWD}/BUILD.hdrs/dst" HOST_OS_VERSION="13.4"
-xcodebuild installhdrs -project libsyscall/Libsyscall.xcodeproj -sdk macosx ARCHS="x86_64" SRCROOT="${PWD}/libsyscall" OBJROOT="${PWD}/BUILD.hdrs/obj" SYMROOT="${PWD}/BUILD.hdrs/sym" DSTROOT="${PWD}/BUILD.hdrs/dst"
-# Set permissions correctly before dittoing over the SDK.
-sudo chown -R root:wheel BUILD.hdrs/dst/
-sudo ditto BUILD.hdrs/dst "${SDKPATH}"
+make SDKROOT=macosx ARCH_CONFIGS="X86_64" installhdrs
+sudo ditto "$PWD/BUILD/dst" "${SDKPATH}"
 cd ..
 
-# Install libplatform headers to ${SDKPATH}/usr/local/include.
-cd libplatform
-sudo ditto "${PWD}/include" "${SDKPATH}/usr/local/include"
-sudo ditto "${PWD}/private"  "${SDKPATH}/usr/local/include"
-cd ..
-
-# Build and install libdispatch's libfirehose_kernel target to
+# Build and install libdispatch's libfirehose target to
 # ${SDKPATH}/usr/local.
 cd libdispatch
-mkdir -p obj sym dst
-xcodebuild install -project libdispatch.xcodeproj -target libfirehose_kernel -sdk macosx ARCHS="x86_64" SRCROOT="${PWD}" OBJROOT="${PWD}/obj" SYMROOT="${PWD}/sym" DSTROOT="${PWD}/dst"
-sudo ditto "${PWD}/dst/usr/local" "${SDKPATH}/usr/local"
+xcodebuild install -sdk macosx ARCHS='x86_64' VALID_ARCHS='x86_64' -target libfirehose_kernel PRODUCT_NAME=firehose_kernel DSTROOT=$PWD/dst
+sudo ditto "$PWD/dst/usr/local" "${SDKPATH}/usr/local"
 cd ..
 
 echo "attempting to build a DEVELOPMENT kernel with all local variables and arguments"
-
-
 cd xnu
-export CFLAGS_DEVELOPMENTARM64="-O0 -g -DKERNEL_STACK_MULTIPLIER=2"
-export CXXFLAGS_DEVELOPMENTARM64="-O0 -g -DKERNEL_STACK_MULTIPLIER=2"
+export CFLAGS_DEVELOPMENTX86_64="-O0 -g -DKERNEL_STACK_MULTIPLIER=2"
+export CXXFLAGS_DEVELOPMENTX86_64="-O0 -g -DKERNEL_STACK_MULTIPLIER=2"
 
-make \
-ARCH_CONFIGS="X86_64" \
+make BUILD_LTO=0 ARCH_CONFIGS="X86_64" \
 KERNEL_CONFIGS=DEVELOPMENT \
-SDK_ROOT=macosx \
--j8
+SDK_ROOT=macosx -j8
 
 echo $(ls -a)
